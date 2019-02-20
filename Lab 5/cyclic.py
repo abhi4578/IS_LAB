e884764890622d0fa0dcd7c0085ee97c59bd5bac
Abhilash V <abhilash9398@gmail.com>
2019-02-20 16:07:52 +0530

    Update cyclic.py

--- Lab 5/cyclic.py
+++ Lab 5/cyclic.py
@@ -9,27 +9,30 @@ def exp_modulo(a,b,N):
     return result%N
 
 def main():
-    cipher=input("Enter the ciphertext \n")
+    #
+    f=open("Program5-Output-CyclicProgram5.txt",'w')
+    ciphertext=int(input("Enter the ciphertext \n"))
     N=int(input("Enter N\n"))
-    e=int(input("Enter e\n"))
-    plaintext=''
-    for i in cipher:
-        temp=ord(i)
-        plain=None
-        c=0
-        while c==0 or temp!=ord(i):
-            c=c+1
-            plain=temp
-            temp=exp_modulo(temp,e,N)
+    e=int(input("Enter public key e\n"))
+    print("Ciphertext:",ciphertext,"N:",N,"public key e:",e,"\n",file=f)
+    plaintext=0
+    temp=ciphertext
+    c=0
+    while c==0 or temp!=ciphertext:
+        c=c+1
+        plaintext=temp
+        
+        temp=exp_modulo(temp,e,N)
+        #print("cyclic No:",c,"intermediate cipher:",temp)
+        print("cyclic No:",c,"intermediate cipher:",temp,file=f)
         #print(c)
-        plaintext=plaintext+chr(plain)
+    
     
     print("plaintext is: ",plaintext)
-    ciphertext=''
-    for i in plaintext:
-        temp=exp_modulo(ord(i),e,N)
-        ciphertext+=chr(temp)
+    print("plaintext is: ",plaintext,file=f)
+    ciphertext=exp_modulo(plaintext,e,N)
     print("encrypting the plain text obtained: ",ciphertext)
+    print("encrypting the plain text obtained: ",ciphertext,file=f)
             
 if __name__ == "__main__":
     main()
